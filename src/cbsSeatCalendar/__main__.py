a4441f6ddb825f5ae5a088d9d4712fad80473be6
EHamre <RegEspenHamre@outlook.com>
2021-11-13 13:36:37 +0100

    fixed entry point mistake

--- src/cbsSeatCalendar/__main__.py
+++ src/cbsSeatCalendar/__main__.py
@@ -1,13 +1,6 @@
-from getpass import getpass
 from . import CBSaccount
 
 
-def executeCBScal():
-	username = input('CBS username: ')
-	password = getpass('Password: ')
-	
-	myAcc = CBSaccount(username, password)
-	myAcc.updateCalendar()
 
 if __name__ == '__main__':
 	executeCBScal()
