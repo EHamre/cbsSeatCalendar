84a017bf39049f271e31c633ff707451b58594b4
EHamre <RegEspenHamre@outlook.com>
2021-11-22 13:10:16 +0100

    removes cancellations from calendar and prints quota for bookings.

--- src/cbsSeatCalendar/__init__.py
+++ src/cbsSeatCalendar/__init__.py
@@ -1,7 +1,7 @@
 # Top-level objects and function for command line entry point
 from cbsSeatCalendar.cbsCalendarUpdateClass import CBSaccount
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 
 def executeCBScal():
